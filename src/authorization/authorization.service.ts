ec9f750cdb886bab698d914fdfc39a4d1c369734
Izequias Silva <master@dedicado.digital>
2024-03-19 14:52:44 -0300

    update

--- src/authorization/authorization.service.ts
+++ src/authorization/authorization.service.ts
@@ -1,13 +1,21 @@
 import { HttpException, Injectable } from '@nestjs/common'
+import { AuthorizationDto } from './authorization.dto'
 
 @Injectable()
 export class AuthorizationService {
   constructor() {}
 
   async validation(authorizationKey: string, request: Request): Promise<any> {
-    const { method, url } = request
+    const { method, url, headers } = request
+    const { host }: any = headers
+    const authorizationDto: AuthorizationDto = {
+      key: authorizationKey,
+      host: host,
+      method: method,
+      url: url,
+    }
     try {
-      console.log(method, url, authorizationKey)
+      console.log(authorizationDto)
       return true
     } catch (error: any) {
       throw new HttpException(error, error.status)
